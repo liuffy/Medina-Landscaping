ce644afbb5ed0c956c730175e31152fbdad4abb9
Sophia Liu <sliu3@wellesley.edu>
2017-04-14 10:29:33 -0700

    Webpack React Sass environment set up

--- webpack.config.js
+++ webpack.config.js
@@ -9,19 +9,31 @@ const path = require('path');
 	    filename: 'bundle.js'
 	  },
 	  resolve: {
-	    extensions: ['', '.js', '.jsx']
+	    extensions: ['.js', '.jsx', '.scss']
 	  },
 	  module: {
 	    loaders: [
 	      {
 	        test: /\.jsx?$/,
 	        exclude: /(node_modules|bower_components)/,
-	        loader: 'babel',
+	        loader: 'babel-loader',
 	        query: {
 	          presets: ['react', 'es2015']
 	        }
-	      }
-	    ]
+	      },
+	      {
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
+	    
 	  },
 	  devtool: 'source-maps'
 	};
+
+
