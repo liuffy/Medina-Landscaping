46b26e0ba6763122d2a8659e727a6b6c0f3bd7f2
Sophia Liu <sliu3@wellesley.edu>
2017-04-17 13:38:55 -0700

    Import fonts in header

--- frontend/components/contact.jsx
+++ frontend/components/contact.jsx
@@ -7,13 +7,24 @@ const Contact = () =>{
 		<div>
 			<div className="col-half info-block">
 			<h3>Contact</h3>
+			<ul>
+			<li>Hi</li>
+			<li>Hi</li>
+			<li>Hi</li>
+			</ul>
 			</div>
 
 			<div className="col-half info-block">
 			<h3>Hours</h3>
+				<ul>
+			<li>Hi</li>
+			<li>Hi</li>
+			<li>Hi</li>
+			</ul>
 			</div>
 			<div className="col-whole info-block">
 			<h3>Insurance</h3>
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 			</div>
 		</div>
 		)
