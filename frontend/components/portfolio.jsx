cc66abc3f918b2b94b883017e5468fa6afa06713
Sophia Liu <sliu3@wellesley.edu>
2017-04-17 18:26:24 -0700

    Custom grid

--- frontend/components/portfolio.jsx
+++ frontend/components/portfolio.jsx
@@ -2,33 +2,31 @@ import React from 'react';
 import '../../css/app';
 import '../../css/portfolio';
 
-
-
 const Portfolio = () =>{
 	return (
 		<div>
 
- <div className="main-content align-center padded">   
-<div className="container-fluid">
+<div className="main-content align-center">   
+  <div className="container-fluid align-center">
 
-                  <div className="open-project-lightbox">
+    <div className="open-project-lightbox">
 
-                  <a href="app/assets/img/img1.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img1.png" /></a>
+      <a href="app/assets/img/img1.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img1.png" /></a>
 
-                  <a href="app/assets/img/img2.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img2.png"/></a>                
+      <a href="app/assets/img/img2.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img2.png"/></a>                
 
-                   <a href="app/assets/img/img3.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img3.png"/></a>
+      <a href="app/assets/img/img3.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img3.png"/></a>
 
-                   <a href="app/assets/img/img4.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img4.png"/></a>
+      <a href="app/assets/img/img4.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img4.png"/></a>
 
-                   <a href="app/assets/img/img4.png" data-lightbox="example-1" ><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img4.png"/></a>
-                           
-                   <a href="app/assets/img/img4.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img4.png"/></a></div>
+      <a href="app/assets/img/img4.png" data-lightbox="example-1" ><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img4.png"/></a>
+
+      <a href="app/assets/img/img4.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img4.png"/></a></div>
 
 
- </div>
- </div>
     </div>
+  </div>
+</div>
 		)
 }
 
