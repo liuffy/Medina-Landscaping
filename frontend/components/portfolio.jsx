ba5cfae6f9aa34ee3b705db1e3662dd1b3f70232
Sophia Liu <sliu3@wellesley.edu>
2017-04-17 17:58:01 -0700

    Replace PNG with SVG

--- frontend/components/portfolio.jsx
+++ frontend/components/portfolio.jsx
@@ -7,18 +7,10 @@ import '../../css/portfolio';
 const Portfolio = () =>{
 	return (
 		<div>
-	 <div className="showbox">
-		<div className="loader">
-        <svg viewBox="25 25 50 50" className="circular">
-          <circle cx="50" cy="50" r="20" fill="none" strokeWidth="6" strokeMiterlimit="10" className="path"></circle>
-        </svg>
-      </div>
-    </div>
-        <div className="container-fluid">
 
-          <div className="row">
-            <div className="container">
-                <div className="row">
+ <div className="main-content align-center padded">   
+<div className="container-fluid">
+
                   <div className="open-project-lightbox">
 
                   <a href="app/assets/img/img1.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img1.png" /></a>
@@ -34,10 +26,8 @@ const Portfolio = () =>{
                    <a href="app/assets/img/img4.png" data-lightbox="example-1"><span className="plus"><span></span><span></span></span><img alt="" src="app/assets/img/img4.png"/></a></div>
 
 
-                </div>
-            </div>
-        </div>
-      </div>
+ </div>
+ </div>
     </div>
 		)
 }
