6e818d9e6c1e96168499883261b28f9445a535d4
Sophia Liu <sliu3@wellesley.edu>
2017-04-17 17:07:34 -0700

    Added icons

--- frontend/components/contact.jsx
+++ frontend/components/contact.jsx
@@ -4,29 +4,32 @@ import '../../css/contact';
 
 const Contact = () =>{
 	return (
- <div className="main-content align-center">		
+ <div className="main-content align-center padded">		
 <div className="container-fluid">
 		<div className="col-md-6 text-center">
 		<h3>Contact</h3>
 		<ul className="info-list">
-		<li>(999) 999 - 9999</li> 
-		<li>medinatree@gmail.com</li>
-		<li>44 Tree Road, <br/> Waltham, MA</li>
+		<li><img className="logo-pic-mini"
+							src="./../app/assets/img/phone-call.svg"/>(999) 999 - 9999</li> 
+		<li><img className="logo-pic-mini"
+							src="./../app/assets/img/mail.svg"/>medinatree@gmail.com</li>
+		<li><img className="logo-pic-mini"
+							src="./../app/assets/img/building.svg"/>44 Tree Road, Waltham, MA</li>
 		</ul>
 		</div>
 
 		<div className="col-md-6 text-center">
 		<h3>Hours</h3>
 		<ul className="info-list">
-		<li>Mon: 9 AM - 7 PM</li>
-		<li>Tues: 9 AM - 7 PM</li>
-		<li>Wed: 9 AM - 7 PM</li>
-		<li>Thurs: 9 AM - 7 PM</li>
-		<li>Fri: 9 AM - 7 PM</li>
-		<li>Sat: 12 PM - 7 PM</li>
+		<li><b>Mon:</b> 9 AM - 7 PM</li>
+		<li><b>Tues:</b> 9 AM - 7 PM</li>
+		<li><b>Wed:</b> 9 AM - 7 PM</li>
+		<li><b>Thurs:</b> 9 AM - 7 PM</li>
+		<li><b>Fri:</b> 9 AM - 7 PM</li>
+		<li><b>Sat:</b> 12 PM - 7 PM</li>
 		</ul>
 		</div>
-		<div className="col-md-8 insurance text-center">
+		<div className="col-md-12 insurance text-center">
 		<h3><img className="logo-pic" src="./../app/assets/img/insurance.svg" />Insurance</h3>
 		<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 		</div>
