f63031a95e61e58a302ae20bd214ede968c61170
Sophia Liu <sliu3@wellesley.edu>
2017-04-18 17:58:21 -0700

    Contact form added

--- frontend/components/contact.jsx
+++ frontend/components/contact.jsx
@@ -6,14 +6,14 @@ const Contact = () =>{
 	return (
 <div className="container">   
  
-		<div className="col-md-6 ">
+		<div className="col-md-5 col-md-offset-1 col-sm-8 col-sm-offset-1 ">
 			<h3>Contact</h3>
 			<ul className="info-list">
 				<li><img className="logo-pic-mini"
 									src="./../app/assets/img/calendar.svg"/>
-<b>Invoice/Scheduling:</b> 508-879-0900<br/>
+<b>Invoice/Scheduling:</b> (508) 879-0900<br/>
 <img className="logo-pic-mini"
-									src="./../app/assets/img/phone-call.svg"/><b>General: </b> 978-333-9334
+									src="./../app/assets/img/phone-call.svg"/><b>General: </b> (978) 333-9334
 </li> 
 				<li><img className="logo-pic-mini"
 									src="./../app/assets/img/mail.svg"/><b>E-mail:</b> medinatreeservice@gmail.com</li>
@@ -25,18 +25,36 @@ const Contact = () =>{
 			</ul>
 		</div>
 
+		 <div className="col-md-5 col-md-offset-1 col-sm-8 col-sm-offset-1 form-wrapper contactform">
+              <h4 className="margin-0 text-1-opacity">Need our help? </h4>
+              <form action="#" method="get" id="contact-form">
+                <div className="form-group name">
+                  <label htmlFor="input-name">Your name</label>
+                  <input type="text" name="name" id="input-name" className="form-control"/>
+                </div>
+                <div className="form-group email">
+                  <label htmlFor="email">Your e-mail</label>
+                  <input type="text" name="email" id="email" className="form-control"/>
+                </div>             
 
-		<div className="col-md-6 ">
-			<h3>Hours</h3>
-			<ul className="info-list">
-				<li><b>Mon:</b> 9 AM - 7 PM</li>
-				<li><b>Tues:</b> 9 AM - 7 PM</li>
-				<li><b>Wed:</b> 9 AM - 7 PM</li>
-				<li><b>Thurs:</b> 9 AM - 7 PM</li>
-				<li><b>Fri:</b> 9 AM - 7 PM</li>
-				<li><b>Sat:</b> 12 PM - 7 PM</li>
-			</ul>
-		</div>
+                <div className="form-group phone">
+                  <label htmlFor="phone">Your phone number</label>
+                  <input type="text" name="phone" id="phone" className="form-control"/>
+                </div>
+
+               <div className="form-group address">
+                  <label htmlFor="address">Where are you located?</label>
+                  <input type="text" name="address" id="address" className="form-control"/>
+                </div>
+
+                <div className="form-group text-area message">
+                  <label htmlFor="message">Describe the service needed</label>
+                  <textarea rows="3" name="msg" id="message" className="scroll form-control"></textarea>
+                </div>
+                <button type="submit" className="btn btn-primary submit-btn">Request our services</button>
+              </form>
+              <div className="contact-help-block"></div>
+            </div>
 
 	</div>
 		)
